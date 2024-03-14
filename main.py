aaacc80348b7d8843f9200cad4c6dd6184b3876a
igoutta <10776042+igoutta@users.noreply.github.com>
2024-03-14 12:05:29 -0500

    Servidor Simple

--- main.py
+++ main.py
@@ -4,38 +4,55 @@ from time import sleep
 from multiprocessing import Process
 import warnings
 import sys
+import http.server
+import socketserver
+
+
+# here you create a new handler, you had a new way to handle get request
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/":
+            self.path = "/index.html"
+        # this code execute when a GET request happen, then you have to check if the request happenned because the user pressed the button
+        if self.path.find("Start=true") != -1 and not main_process.is_alive():
+            main_process.start()
+        if self.path.find("Stop=true") != -1 and not blocking:
+            stop()
+        # do whatever you want
+        return super().do_GET()
 
 
 def main():
-    print("Activando bomba y cerrando valvulas de desfogue")
-    bomba_llenado.on()
-    print("Esperar que se llene el tanque para hacer separación")
-    TIEMPO_LLENADO = 15
-    sensor_limite.wait_for_press(timeout=TIEMPO_LLENADO)
-    print("Inicia la clasificación")
-    bomba_llenado.off()
-    bomba_corriente.on()
-    motor_nema.forward(speed=0.95)  # 0.9 es lo óptimo
-    TIEMPO_LIMPIEZA_SEC = 60
-    sleep(TIEMPO_LIMPIEZA_SEC)
-    print("Inicia el desfogue por el tubo de escape")
-    motor_nema.stop()
-    sleep(1)
-    servo_sapo.max()
-    TIEMPO_DESFOGUE_SEC = 15
-    sleep(TIEMPO_DESFOGUE_SEC)
-    print("Cierra el tubo de escape y apaga la corriente")
-    bomba_corriente.off()
-    servo_sapo.min()
-    sleep(1)
-    servo_sapo.detach()
-    print("Limpia residuos del tubo")
-    servo_tunel.min()
-    sleep(1.7)
-    servo_tunel.max()
-    sleep(0.95)
-    servo_tunel.detach()  # Para asegurar que este apagado
-    print("Termina el proceso")
+    while True:
+        print("Activando bomba y cerrando valvulas de desfogue")
+        bomba_llenado.on()
+        print("Esperar que se llene el tanque para hacer separación")
+        TIEMPO_LLENADO = 15
+        sensor_limite.wait_for_press(timeout=TIEMPO_LLENADO)
+        print("Inicia la clasificación")
+        bomba_llenado.off()
+        bomba_corriente.on()
+        motor_nema.forward(speed=0.95)  # 0.9 es lo óptimo
+        TIEMPO_LIMPIEZA_SEC = 60
+        sleep(TIEMPO_LIMPIEZA_SEC)
+        print("Inicia el desfogue por el tubo de escape")
+        motor_nema.stop()
+        sleep(2)
+        servo_sapo.max()
+        TIEMPO_DESFOGUE_SEC = 15
+        sleep(TIEMPO_DESFOGUE_SEC)
+        print("Cierra el tubo de escape y apaga la corriente")
+        bomba_corriente.off()
+        servo_sapo.min()
+        sleep(1)
+        servo_sapo.detach()
+        print("Limpia residuos del tubo")
+        servo_tunel.min()
+        sleep(1.7)
+        servo_tunel.max()
+        sleep(0.95)
+        servo_tunel.detach()  # Para asegurar que este apagado
+        print("Termina el proceso")
 
 
 def stop():
@@ -77,10 +94,13 @@ NEMA_DIR = "J8:5"
 # *PWM
 SAPO = "J8:33"
 TUNEL = "J8:32"
+# *Server
+PORT = 8080
+myHandler = Handler
 
 # Instanciación de pines
-boton_activar = Button(pin=INICIAR, bounce_time=5)
-boton_parar = Button(pin=PARAR, bounce_time=5)
+boton_activar = Button(pin=INICIAR)
+boton_parar = Button(pin=PARAR)
 sensor_limite = Button(pin=FIN_CARRERA, pull_up=False, bounce_time=5)
 
 servo_sapo = AngularServo(pin=SAPO, initial_angle=0)
@@ -90,8 +110,8 @@ servo_tunel.detach()  # Importante
 
 motor_nema = PhaseEnableMotor(phase=NEMA_DIR, enable=NEMA_STEP)
 
-bomba_llenado = LED(pin=RELE_LLENADO)
-bomba_corriente = LED(pin=RELE_CORRIENTE)
+bomba_llenado = LED(RELE_LLENADO)
+bomba_corriente = LED(RELE_CORRIENTE)
 
 # Instancias de procesamiento
 blocking = False
@@ -101,11 +121,13 @@ try:
     signal(SIGTERM, safe_exit)
     signal(SIGHUP, safe_exit)
 
-    boton_parar.when_pressed = stop
-    if boton_activar.is_pressed and not main_process.is_alive() and not blocking:
-        main_process.start()
+    # boton_parar.when_pressed = stop
+    # if boton_activar.is_pressed and not main_process.is_alive() and not blocking:
+    #    main_process.start()
 
-    pause()
+    with socketserver.TCPServer(("", PORT), myHandler) as httpd:
+        print("serving at port", PORT)
+        httpd.serve_forever()
 
 except KeyboardInterrupt:
     if main_process.is_alive():
