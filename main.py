ec72413375b27ef88831dca7dda412645b4a47d5
igoutta <10776042+igoutta@users.noreply.github.com>
2024-03-14 08:09:19 -0500

    Actualización de servos lista.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from signal import signal, SIGTERM, SIGHUP, pause
 from gpiozero import Button, LED, Servo, AngularServo, PhaseEnableMotor
 from time import sleep
 from multiprocessing import Process
+import warnings
 import sys
 
 
@@ -11,12 +12,12 @@ def main():
     servo_sapo.mid()
     servo_tunel.mid()
     print("Esperar que se llene el tanque para hacer separaración")
-    TIEMPO_LLENADO = 8
+    TIEMPO_LLENADO = 15
     sensor_limite.wait_for_press(timeout=TIEMPO_LLENADO)
     print("Inicia la clasificación")
     bomba_llenado.off()
     bomba_corriente.on()
-    motor_nema.forward(speed=0.99)  # 0.99 es llo máximo
+    motor_nema.forward(speed=0.999)  # 0.999 es lo óptimo
     TIEMPO_LIMPIEZA_SEC = 120
     sleep(TIEMPO_LIMPIEZA_SEC)
     print("Inicia el desfogue por el tubo de escape")
@@ -30,29 +31,38 @@ def main():
     TIEMPO_FINAL_SEC = 20
     sleep(TIEMPO_FINAL_SEC)
     print("Limpia residuos del tubo")
-    servo_tunel.max()
-    sleep(1)
+    servo_tunel.min()
+    sleep(1.7)
     print("Termina el proceso")
-    servo_tunel.mid()
-    sleep(0.5)
+    servo_tunel.max()
+    sleep(1.1)
+    servo_tunel.detach()  # Para asegurar que este apagado
 
 
 def stop():
-    print("Termina subidamente el subproceso principal")
-    main_process.kill()
-    print("Apaga todos los actuadores")
-    bomba_llenado.off()
-    bomba_corriente.off()
-    motor_nema.stop()
-    servo_sapo.mid()
-    sleep(5)
-    servo_tunel.mid()
-    sleep(0.5)
+    blocking = True
+    while blocking:
+        print("Termina subidamente el subproceso principal")
+        main_process.kill()
+        print("Apaga todos los actuadores")
+        bomba_llenado.off()
+        bomba_corriente.off()
+        motor_nema.stop()
+        servo_sapo.min()
+        sleep(2)
+        servo_tunel.min()
+        sleep(1.7)
+        servo_tunel.max()
+        sleep(1.1)
+        servo_tunel.detach()
+        blocking = False
 
 
 def safe_exit(signum, frame):
     sys.exit(1)
 
+
+warnings.filterwarnings("ignore")
 
 # Declaración de pines
 INICIAR = "J8:37"
@@ -66,33 +76,30 @@ NEMA_DIR = "J8:5"
 SAPO = "J8:33"
 TUNEL = "J8:32"
 
-# Instanciación de variables
-boton_activar = Button(pin=INICIAR, bounce_time=0.3)
-boton_parar = Button(pin=PARAR, bounce_time=0.3)
-sensor_limite = Button(pin=FIN_CARRERA, pull_up=False, bounce_time=0.3)
+# Instanciación de pines
+boton_activar = Button(pin=INICIAR, bounce_time=2)
+boton_parar = Button(pin=PARAR, bounce_time=2)
+sensor_limite = Button(pin=FIN_CARRERA, pull_up=False, bounce_time=5)
 
 servo_sapo = AngularServo(pin=SAPO, initial_angle=0)
 servo_tunel = Servo(pin=TUNEL, initial_value=0)
+servo_tunel.detach()  # Importante
 
 motor_nema = PhaseEnableMotor(phase=NEMA_DIR, enable=NEMA_STEP)
 
 bomba_llenado = LED(pin=RELE_LLENADO)
 bomba_corriente = LED(pin=RELE_CORRIENTE)
 
-# Procesamiento
+# Instancias de procesamiento
+blocking = False
 main_process: Process = Process(target=main, name="Principal")
-
 
 try:
     signal(SIGTERM, safe_exit)
     signal(SIGHUP, safe_exit)
 
     boton_parar.when_pressed = stop
-    if (
-        boton_activar.is_pressed
-        and not main_process.is_alive()
-        and not boton_parar.is_pressed
-    ):
+    if boton_activar.is_pressed and not main_process.is_alive() and not blocking:
         main_process.start()
 
     pause()
@@ -106,14 +113,9 @@ finally:
         main_process.terminate()
     main_process.close()
     print("Finaliza todas las conexiones")
-    bomba_llenado.off()
-    bomba_corriente.off()
     bomba_llenado.close()
     bomba_corriente.close()
-    motor_nema.stop()
     motor_nema.close()
-    servo_sapo.mid()
-    servo_tunel.mid()
     servo_sapo.close()
     servo_tunel.close()
     sensor_limite.close()
--- src/tools/config_servos.py
+++ src/tools/config_servos.py
@@ -1,9 +1,13 @@
 from gpiozero import AngularServo, Servo
+from signal import pause
 
 # from gpiozero.pins.pigpio import PiGPIOFactory
 from time import sleep
+import warnings
 
-SAPO = 13
+warnings.filterwarnings("ignore")
+
+SAPO = "J8:33"
 TUNEL = "J8:32"
 
 # Declaración de pines
@@ -12,11 +16,17 @@ servo_sapo = AngularServo(pin=SAPO, initial_angle=0)  # pin_factory=factory)
 servo_tunel = Servo(pin=TUNEL, initial_value=0)  # pin_factory=factory)
 
 try:
-    servo_tunel.min()
-    sleep(1.1)
-    servo_tunel.max()
-    sleep(0.95)
-
+    while True:
+        servo_tunel.detach()
+        servo_sapo.min()
+        sleep(2)
+        servo_sapo.max()
+        sleep(2)
+        servo_tunel.min()
+        sleep(1.7)
+        servo_tunel.max()
+        sleep(1.1)
+        servo_tunel.detach()
 except KeyboardInterrupt:
     exit(1)
 
